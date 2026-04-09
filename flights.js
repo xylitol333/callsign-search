f969bbef5fdd55cd93faadcc1c6ffbe78d22351b
xylitol333 <71183763+xylitol333@users.noreply.github.com>
2026-04-09 22:07:07 +0900

    Delete flights.js

--- flights.js
+++
@@ -1,5 +0,0 @@
-const flights = [
-  { airline: "제주항공", flight: "7C101", from: "서울/김포", to: "제주" },
-  { airline: "제주항공", flight: "7C1353", from: "부산", to: "간사이" },
-  { airline: "제주항공", flight: "7C1461", from: "무안", to: "나가사키" }
-];
