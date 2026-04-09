58f7c97a522b6e50dc4a6247a117cf26a1bc7aed
xylitol333 <71183763+xylitol333@users.noreply.github.com>
2026-04-09 22:07:34 +0900

    Create flights.js

---
+++ flights.js
@@ -0,0 +1,6 @@
+window.flights = [
+  { airline: "제주항공", flight: "7C101", from: "서울/김포", to: "제주" },
+  { airline: "제주항공", flight: "7C1353", from: "부산", to: "간사이" },
+  { airline: "제주항공", flight: "7C1354", from: "간사이", to: "부산" },
+  { airline: "제주항공", flight: "7C1461", from: "무안", to: "나가사키" }
+];
